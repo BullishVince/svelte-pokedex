7a2f5f75e6341c80566615490adafdbd43ba5b25
BullishVince <vincent@bullishlife.com>
2021-11-04 22:46:36 +0100

    display the sprite for each pokemon

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,10 +16,24 @@
     @import 'tailwindcss/dist/tailwind.css';
 </style>
 
-<div class="container mt-8">
+<div class="container py-8">
     <ul class="grid sm:grid-cols-2 md:grid-cols-3 sm:col-gap-6 row-gap-4">
-        {#each pokemons as { name, url }, index (index)}
-        <li><p class="font-bold text-xl mb-2">{name}</p></li>
-        {/each}
+      {#each pokemons as { name, url }, index (index)}
+      <li>
+        <article
+          class="flex flex-col items-center max-w-sm rounded overflow-hidden
+          shadow-lg"
+        >
+          <div class="px-6 py-4">
+            <h2 class="font-bold text-xl mb-2">{name}</h2>
+          </div>
+          <img
+            class="w-24 h-24"
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{index + 1}.png"
+            alt=""
+          />
+        </article>
+      </li>
+      {/each}
     </ul>
-</div>
+  </div>
