e53534c8972d3857abd266cb10cb59d89fa3ef64
BullishVince <vincent@bullishlife.com>
2021-11-07 03:03:49 +0100

    SSR all pokemon data before rendering page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,21 @@
+<script context="module">
+  export async function load({page}){
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=${151}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    const loadedPokemon = data.results.map((data, index) => {
+        return {
+            name: data.name,
+            id: index + 1,
+            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+        };
+    });
+    return {props: { pokemon:loadedPokemon}};
+  }
+</script>
 <script>
-  import {pokemon} from '../stores/pokestore';
   import PokemanCard from '../components/pokemanCard.svelte';
+  export let pokemon; //Loaded through SSR in async function load
 
   let searchTerm = "";
   let filteredPokemon = [];
@@ -8,9 +23,9 @@
   $: {
     if (searchTerm) {
       //Search for pokemon
-      filteredPokemon = $pokemon.filter(pokeman => pokeman.name.toLowerCase().includes(searchTerm.toLowerCase()));
+      filteredPokemon = pokemon.filter(pokeman => pokeman.name.toLowerCase().includes(searchTerm.toLowerCase()));
     } else {
-      filteredPokemon = [...$pokemon];
+      filteredPokemon = [...pokemon];
     }
   }
 </script>
